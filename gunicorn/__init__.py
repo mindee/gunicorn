5c9b24cc741ca664741b6fa946d1dec3dac48131
bashir ABDEL WAHED <bashir@mindee.co>
2024-08-21 13:49:14 +0200

    fix: :bug: gunicorn load balance requests between workers

--- gunicorn/__init__.py
+++ gunicorn/__init__.py
@@ -4,6 +4,6 @@
 # See the NOTICE for more information.
 
 version_info = (23, 0, 0)
-__version__ = ".".join([str(v) for v in version_info])
+__version__ = ".".join([str(v) for v in version_info]) + "post1"
 SERVER = "gunicorn"
 SERVER_SOFTWARE = "%s/%s" % (SERVER, __version__)
